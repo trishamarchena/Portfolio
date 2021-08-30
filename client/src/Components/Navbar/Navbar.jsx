6bc577508819a503aa37baab622c379bd9f36460
trishamarchena <galadrielvasquez@gmail.com>
2021-08-30 11:18:43 -0400

    added projects images

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
   return (
     <nav>
     <div className ="navbarContainer">
+      {/* < h2 className="myLogo">Patricia Marchena</h2> */}
       <Link to="/Homepage">Home</Link>
       <a href ="#aboutContainer" className="aboutLink">About</a>
       <a href ="#projectContainer" className="projects">Projects</a>
--- client/src/Components/Projects/Projects.jsx
+++ client/src/Components/Projects/Projects.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import "./projects.css"
 
 const Projects = () => {
   return (
     <div id="projectContainer">
-      
+      <div className="projectInfo">
+      <h2 className="projectHeader">My Projects</h2>
+      <div className="projectDetails">
+      <img
+            alt="feed me app"
+            id="feedMeProject"
+            src={require("./feedme.png").default}
+          ></img>
+          <p className="projectDescription">The Feed me App, </p>
+          <img
+            alt="mending minds app"
+            id="mendingMindsProject"
+            src={require("./mendingmind.png").default}
+          ></img>
+          <p className="projectDescription">Mending Mind App </p>
+      </div>
+      </div>
     </div>
   );
 };
