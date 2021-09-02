615b13cdbebdb723f429155105a88421a5f8f042
trishamarchena <galadrielvasquez@gmail.com>
2021-09-02 11:42:33 -0400

    COmpleted the contact section functionality, Emails are now forwarded to my account. Added Favicon and Title to the page

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -1,35 +1,75 @@
+import { send } from "emailjs-com";
+import { useState } from "react";
 import React from "react";
 import "./Contact.css";
 
 const Contact = () => {
+  const [toSend, setToSend] = useState({
+    from_name: "",
+    to_name: "",
+    message: "",
+    reply_to: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_wc646qj',
+      'template_u5xga8e',
+      toSend,
+      'user_9GPXvwXdItUpwaCBE1LQD'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  }
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
   return (
     <div id="contactMeContainer">
       <h2 className="contactTitle">Contact</h2>
-      <label htmlFor="inputname"></label>
-      <input
-        type="text"
-        placeholder="Your beautiful name"
-        id="inputname"
-        required
-        name="inputname"
-      />
-      <label htmlFor="inputemail"></label>
-      <input
-        type="email"
-        placeholder="Email"
-        id="inputemail"
-        required
-        name="inputemail"
-      />
-      <label htmlFor="inputmessage"></label>
-      <textarea
-        type="text"
-        placeholder="Your message"
-        id="inputmessage"
-        required
-        name="inputmessage"
-      />
-      <button className ="contactButton"> Submit </button>
+      <form onSubmit={onSubmit}>
+        <input
+          id="inputname"
+          type="text"
+          name="from_name"
+          placeholder="Your beautiful name"
+          value={toSend.from_name}
+          required
+          onChange={handleChange}
+          />
+        <input
+          type="text"
+          name="to_name"
+          placeholder="Patricia"
+          value={toSend.to_name}
+          onChange={handleChange}
+          />
+          <input
+            id="inputemail"
+            type="text"
+            name="reply_to"
+            placeholder="Your email"
+            value={toSend.reply_to}
+            required
+            onChange={handleChange}
+            />
+        <textarea
+          id="inputmessage"
+          type="text"
+          name="message"
+          placeholder="Your message"
+          value={toSend.message}
+          required
+          onChange={handleChange}
+        />
+        <button type="Submit" className="contactButton">Submit</button>
+      </form>
     </div>
   );
 };
