ab630ef0f75d722db512b0d6345092e66d85aa53
trishamarchena <galadrielvasquez@gmail.com>
2021-09-17 23:43:36 -0400

    Added SPF Project added links and added links to github and apps for the projects

--- client/src/Components/Projects/Projects.jsx
+++ client/src/Components/Projects/Projects.jsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import "./projects.css"
+import React from "react";
+import "./projects.css";
 
 const Projects = () => {
   return (
     <div id="projectContainer">
       <div className="projectInfo">
-      <h2 className="projectHeader">My Projects</h2>
-      <div className="projectDetails">
-      <img
+        <h2 className="projectHeader">My Projects</h2>
+        <div className="projectDetails">
+          <img
             alt="feed me app"
             id="feedMeProject"
             src={require("./feedme.png").default}
           ></img>
-          <p className="projectDescription">The Feed me App, </p>
+          <p className="projectTitle">The Feed me App </p>
+          <div className="githublinkContainer">
+            <a
+              target="_blank "
+              href="https://github.com/trishamarchena/Feed-me-app/tree/main"
+            >
+              <p className="githubLink"> Take me to the code! </p>
+            </a>
+          </div>
+          <div className="weblinkContainer">
+            <a target="_blank " href="https://feed-me-app.netlify.app/">
+              <p className="webLink"> Take me to the website! </p>
+            </a>
+          </div>
+
           <img
             alt="mending minds app"
             id="mendingMindsProject"
             src={require("./mendingmind.png").default}
           ></img>
-          <p className="projectDescription">Mending Mind App </p>
-      </div>
+          <p className="projectTitle">Mending Mind App </p>
+          <div className="githublinkContainer">
+            <a
+              target="_blank "
+              href="https://github.com/trishamarchena/mending-minds"
+            >
+              <p className="githubLink"> Take me to the code!</p>
+            </a>
+          </div>
+          <div className="weblinkContainer">
+            <a a target="_blank " href=" https://mending-minds.netlify.app/">
+              <p className="webLink"> Take me to the website!</p>
+            </a>
+          </div>
+          <img
+            alt="spf app"
+            id="spfProject"
+            src={require("./spf.png").default}
+          ></img>
+          <p className="projectTitle">SPF App </p>
+          <div className="githublinkContainer">
+            <a target="_blank " href="https://github.com/trishamarchena/SPF">
+              <p className="githubLink"> Take me to the code!</p>
+            </a>
+          </div>
+          <div className="weblinkContainer">
+            <a a target="_blank " href="https://spf-sth.netlify.app/">
+              <p className="webLink"> Take me to the website!</p>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
